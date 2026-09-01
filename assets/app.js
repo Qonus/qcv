4240a343f427bcf12580f1a75b3475adc1db8f69
Qonus <a1batkonus1@gmail.com>
2026-09-01 15:53:24 +0500

    Add custom bootstrap form layout

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,6 @@
  * which should already be in your base.html.twig.
  */
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import 'bootstrap';
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,19 +18,8 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue(
-                        message: 'You should agree to our terms.',
-                    ),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank(
                         message: 'Please enter a password',
@@ -40,6 +29,14 @@ class RegistrationFormType extends AbstractType
                         minMessage: 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         max: 4096,
+                    ),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue(
+                        message: 'You should agree to our terms.',
                     ),
                 ],
             ])
