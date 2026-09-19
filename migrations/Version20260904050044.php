4236870f834157418936d1ce7ba3b1fb760f4443
Qonus <a1batkonus1@gmail.com>
2026-09-19 14:43:35 +0500

    Fix migration fatal double quote

--- migrations/Version20260904050044.php
+++ migrations/Version20260904050044.php
@@ -20,7 +20,7 @@ final class Version20260904050044 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE "user" ADD locale VARCHAR(5) DEFAULT "en" NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD locale VARCHAR(5) DEFAULT \'en\' NOT NULL');
     }
 
     public function down(Schema $schema): void
